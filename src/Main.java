1f5c1b65d949e3fed1ee21c52996a5a13dc63d72
Jiří Loukota <jloukota@seznam.cz>
2024-01-21 08:36:57 +0100

    It´s done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.math.BigDecimal;
+import java.util.Date;
+import java.net.InetAddress;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String firstname = "Jiří";//Jméno
+        String surname = "Loukota";//Přímení
+        Date dateOfBirth = new Date(1974, 2, 16);//Datum narození
+        int numberOfContracts = 10;//Počet smluv
+        double weightInTons = 8.5;//Hmotnost mrkve
+        String residence = "Praha";//Sídlo
+        String spz = "3FG6023";//SPZ
+        double consumption = 8.25;//Spořeba auta na 100km
+        String ip4 = "123.45.678.9";//ip adresa v4
+
+        System.out.println("Množství prodané mrkve na jednu smlouvu:" + weightInTons/numberOfContracts + "tun");
+
     }
 }
